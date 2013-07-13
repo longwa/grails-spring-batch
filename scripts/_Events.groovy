c76c09f1ea6637d83674deed480a74da07a9729b
John Engelman <john.r.engelman@gmail.com>
2013-07-13 12:49:42 -0500

    Fix error in _Events.groovy when accessing file (Issue #13)

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,6 +1,6 @@
 if (grailsAppName != 'spring-batch') { //don't do this when compiling just the plugin.
     eventCompileStart = {
-        if (File("${basedir}/grails-app/batch").exists()) {
+        if (new File("${basedir}/grails-app/batch").exists()) {
             ant.copy(todir:"$classesDirPath/batch", preservelastmodified:true) {
                 fileset(dir:"${basedir}/grails-app/batch", includes:"**/*BatchConfig.groovy")
             }
