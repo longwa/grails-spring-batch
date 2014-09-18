e37274c6cc0170be1efe67602364aa136ea1f55d
Daniel Bower <daniel.d.bower@gmail.com>
2014-09-18 09:34:47 -0400

    GitHub#25 - Fix - Compatibility with Grails 2.4.3

--- grails-app/services/grails/plugins/springbatch/SpringBatchService.groovy
+++ grails-app/services/grails/plugins/springbatch/SpringBatchService.groovy
@@ -22,7 +22,7 @@ import org.springframework.context.ApplicationEvent
 import org.springframework.context.ApplicationListener
 import org.springframework.context.event.ContextClosedEvent
 
-class SpringBatchService implements  ApplicationListener {
+class SpringBatchService implements  ApplicationListener<ApplicationEvent> {
 	
 	static transactional = false
 	
