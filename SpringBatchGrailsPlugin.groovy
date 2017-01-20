0a6b1dbff8990ebab374aa7da5f61103b5b5e92a
Aaron Long <along@triu.com>
2017-01-20 17:41:34 -0500

    Add jobExecutionId to the map on launch

--- SpringBatchGrailsPlugin.groovy
+++ SpringBatchGrailsPlugin.groovy
@@ -27,7 +27,7 @@ import org.springframework.batch.core.repository.dao.AbstractJdbcBatchMetadataDa
 
 
 class SpringBatchGrailsPlugin {
-    def version = "2.5.1"
+    def version = "2.5.2"
     def grailsVersion = "2.5 > *"
     def title = "Grails Spring Batch Plugin"
     def author = "John Engelman"
--- grails-app/services/grails/plugins/springbatch/SpringBatchService.groovy
+++ grails-app/services/grails/plugins/springbatch/SpringBatchService.groovy
@@ -1,6 +1,8 @@
 package grails.plugins.springbatch
 
+import grails.transaction.Transactional
 import groovy.sql.Sql
+import org.springframework.transaction.annotation.Propagation
 
 import javax.sql.DataSource
 
@@ -22,255 +24,251 @@ import org.springframework.context.ApplicationEvent
 import org.springframework.context.ApplicationListener
 import org.springframework.context.event.ContextClosedEvent
 
-class SpringBatchService implements  ApplicationListener<ApplicationEvent> {
-	
-	static transactional = false
-	
-	GrailsApplication grailsApplication
-	
-	/**
-	 * true when the grails app has added dynamic methods to classes
-	 */
-	boolean ready = false
-	
-	/**
-	 * Asynchronous Job launcher - default - used for controllers
-	 */
-	JobLauncher jobLauncher
-	
-	/**
-	 * Synchronous Job launcher - used for quartz jobs
-	 */
-	JobLauncher syncJobLauncher
-	
-	/**
-	 * The spring bean name of the the job launcher to use by default
-	 */
-	String defaultJobLauncher = 'jobLauncher'
-	
-	
-	JobRegistry jobRegistry
-	JobService jobService
-	JobOperator jobOperator
-	
-	/**
-	 * Fetch the JobInstance by id
-	 */
-	JobInstance jobInstance(Long jobInstanceId){
-		jobService.getJobInstance(jobInstanceId)
-	}
-	
-	/**
-	 * Fetch the JobExecution by id
-	 */
-	JobExecution jobExecution(Long jobExecutionId){
-		jobService.getJobExecution(jobExecutionId)
-	}
-	
-	/**
-	 * Fetch the JobExecution by id
-	 */
-	StepExecution stepExecution(Long jobExecutionId, Long stepExecutionId){
-		jobService.getStepExecution(jobExecutionId, stepExecutionId)
-	}
-	
-	Collection<StepExecution> previousStepExecutions(String jobName, String stepName, int start, int max){
-		jobService.listStepExecutionsForStep(jobName, stepName, start, max)
-	}
-	
-	/**
-	 * Restart the JobExecution
-	 */
-	void restart(Long jobExecutionId){
-		jobService.restart(jobExecutionId)
-	}
+@Transactional(propagation = Propagation.SUPPORTS)
+class SpringBatchService implements ApplicationListener<ApplicationEvent> {
+    GrailsApplication grailsApplication
 
-	/**
-	 * Stop the JobExecution
-	 */
-	void stop(Long jobExecutionId){
-		jobService.stop(jobExecutionId)
-	}
+    /**
+     * true when the grails app has added dynamic methods to classes
+     */
+    boolean ready = false
 
-	/**
-	 * Stop all JobExecutions for a Job by name
-	 */
-	void stopAllJobExecutions(String jobName){
-		Set<Long> executions = jobOperator.getRunningExecutions(jobName)
-		log.info("Attempting to stop ${executions.size()} job executions for $jobName")
-		
-		executions.each{
-			try{
-				jobOperator.stop(it)
-			}catch(Exception e){
-				log.debug(e)
-			}
-		}
-	}
-	
-	void stopAllJobExecutions(){
-		Collection<String> jobs = jobService.listJobs(0, jobService.countJobs())
-		jobs.each{
-			if(hasRunningExecutions(it)){
-				stopAllJobExecutions(it)
-			}
-		}
-	}
-	
-	/**
-	 * True if there are any running executions for the Job
-	 */
-	boolean hasRunningExecutions(String jobName) {
-		
-		try {
-			//Set<Long> executions = jobOperator.getRunningExecutions(jobName)
-			//return !(executions?.isEmpty())
-			
-			def result = batchSql.firstRow(
-"""select count(bje.job_execution_id) as "executionCount" 
-from ${batchTablePrefix}job_execution bje
-  inner join ${batchTablePrefix}job_instance bji on bje.job_instance_id = bji.job_instance_id
-where bji.job_name = ?
-  and bje.status in ('STARTING', 'STARTED')""".toString(), [jobName])
-			return result['executionCount'] > 0
-						
-		}catch(Exception nsje) {
-			log.info("Failed to acquire running executions for $jobName", nsje)
-			return false
-		}
-	}
-	
-	/**
-	 * Launch a job
-	 *
-	 * @param jobName
-	 * @param canBeConcurrent - when set to false, will look to see if this job
-	 * is already executing and abort launch.  When true, it will always launch a new job
-	 * @param jobParams
-	 * @param jobLauncherName - initially configured choices "jobLauncher" and "syncJobLauncher"
-	 * @return map containing whether the job was successful, and a message regarding either why it failed or what job was started
-	 */
-	Map launch(String jobName, boolean canBeConcurrent=true, 
-			JobParameters jobParams = null, String jobLauncherName = null) {
-		
-		// Is the app ready?
-		if(!ready) {
-			return [success:false, 
-					message:"Attempted to launch $jobName, but app is not ready or job processing is disabled.",
-					failurePriority: 'low']
-		}
-		
-		// Select Job
-		Job job
-		try{
-			job = jobRegistry.getJob(jobName)
-		}catch(NoSuchJobException nsje){
-			return [success: false, message:"Did not find Spring Batch Job: $jobName",
-					failurePriority: 'high']
-		}
-		
-		// Can we run more than one at a time?
-		if(!canBeConcurrent && hasRunningExecutions(jobName)) {
-			return [success: false, 
-					message:"Attempted to launch $jobName, but it is currently running.  Aborting launch.",
-					failurePriority: 'low']
-		}
-		
-		// Select Job Launcher
-		JobLauncher selectedLauncher 
-		if(jobLauncherName) {
-			selectedLauncher = grailsApplication.mainContext.getBean(jobLauncherName)
-		}
-		if(!selectedLauncher) {
-			selectedLauncher = grailsApplication.mainContext.getBean(defaultJobLauncher)
-			log.debug "JobLaucher $jobLauncherName selected but not found, trying $defaultJobLauncher"
-		}
-		if(!selectedLauncher) {
-			return [success: false, message:"Invalid jobLauncher $jobLauncherName selected",
-					failurePriority: 'high']
-		}
-		
-		// Select Job Parameters
-		if(!jobParams) {
-			log.debug "No job parameters provided for job, defaulting to date"
-			jobParams = mapToJobParameters(defaultJobParameters())
-		}
-		
-		// Run the Job
-		try{
-			selectedLauncher.run(job, jobParams)
-		}catch(JobInstanceAlreadyCompleteException jiace){
-			return[success: false, message: jiace.message, failurePriority: 'high']
-		}
-		
-		return [success: true, message:"Spring Batch Job($jobName) launched from EtlService"]
-	}
-	
-	/**
-	 * Job parameters to use by default
-	 */
-	Map defaultJobParameters() {
-		['date': new JobParameter(new Date().dateTimeString)]
-	}
-	
-	/**
-	 * Turn map of job parameters into Spring Batch JobParametersModel
-	 */
-	JobParameters mapToJobParameters(Map map) {
-		return new JobParameters(map)
-	}
-	
-	/**
-	 * For job x, give me the last time it ran and whether it failed or not
-	 * 
-	 * This method gives some of the same info as the SpringBatchUiService.jobModel,
-	 * but might be more useful for automated monitoring.
-	 */
-	Map jobStatus(String jobName) {
-		List<JobExecution> mostRecentJobExecutions = jobService.listJobExecutionsForJob(jobName, 0, 1)
-		if(!mostRecentJobExecutions) {
-			return [success:false]
-		}
-		
-		JobExecution mostRecent = mostRecentJobExecutions[0]
-		
-		boolean success = !mostRecent.status.isUnsuccessful()
-		boolean running = mostRecent.status.isRunning()
-		Date executionStartTime = mostRecent.startTime
-		Date executionEndTime = mostRecent.endTime
-		
-		return [success:success, running:running, executionStartTime:executionStartTime, executionEndTime: executionEndTime]
-	}
+    /**
+     * The spring bean name of the the job launcher to use by default
+     */
+    String defaultJobLauncher = 'jobLauncher'
 
-	/**
-	 * Spring event listener to detect when the app is ready for job launching
-	 */
-	@Override
-	void onApplicationEvent(ApplicationEvent event) {
-		if(event instanceof GrailsContextEvent) {
-			ready = true
-		}
-		if(event instanceof ContextClosedEvent) {
-			ready = false
-			log.info("No longer accepting Batch Jobs, shutting down.")
-		}
-	}
-	
-	
-	private DataSource _batchDataSource
-	private DataSource getBatchDataSource() {
-		_batchDataSource = _batchDataSource ?: grailsApplication.mainContext.getBean(grailsApplication.config.plugin.springBatch.dataSource ?: "dataSource")
-		return _batchDataSource
-	}
-	private String _batchTablePrefix
-	private String getBatchTablePrefix() {
-		if(null==_batchTablePrefix) {
-			_batchTablePrefix = grailsApplication.config.plugin.springBatch.tablePrefix ? (grailsApplication.config.plugin.springBatch.tablePrefix + '_') : 'BATCH_'
-		}
-		return _batchTablePrefix
-	}
-	
-	private Sql getBatchSql() {
-		return new Sql(batchDataSource)
-	}
+    JobRegistry jobRegistry
+    JobService jobService
+    JobOperator jobOperator
+
+    /**
+     * Fetch the JobInstance by id
+     */
+    JobInstance jobInstance(Long jobInstanceId) {
+        jobService.getJobInstance(jobInstanceId)
+    }
+
+    /**
+     * Fetch the JobExecution by id
+     */
+    JobExecution jobExecution(Long jobExecutionId) {
+        jobService.getJobExecution(jobExecutionId)
+    }
+
+    /**
+     * Fetch the JobExecution by id
+     */
+    StepExecution stepExecution(Long jobExecutionId, Long stepExecutionId) {
+        jobService.getStepExecution(jobExecutionId, stepExecutionId)
+    }
+
+    Collection<StepExecution> previousStepExecutions(String jobName, String stepName, int start, int max) {
+        jobService.listStepExecutionsForStep(jobName, stepName, start, max)
+    }
+
+    /**
+     * Restart the JobExecution
+     */
+    void restart(Long jobExecutionId) {
+        jobService.restart(jobExecutionId)
+    }
+
+    /**
+     * Stop the JobExecution
+     */
+    void stop(Long jobExecutionId) {
+        jobService.stop(jobExecutionId)
+    }
+
+    /**
+     * Stop all JobExecutions for a Job by name
+     */
+    void stopAllJobExecutions(String jobName) {
+        Set<Long> executions = jobOperator.getRunningExecutions(jobName)
+        log.info("Attempting to stop ${executions.size()} job executions for $jobName")
+
+        executions.each {
+            try {
+                jobOperator.stop(it)
+            }
+            catch (Exception e) {
+                log.debug(e)
+            }
+        }
+    }
+
+    void stopAllJobExecutions() {
+        Collection<String> jobs = jobService.listJobs(0, jobService.countJobs())
+        jobs.each {
+            if (hasRunningExecutions(it)) {
+                stopAllJobExecutions(it)
+            }
+        }
+    }
+
+    /**
+     * True if there are any running executions for the Job
+     */
+    boolean hasRunningExecutions(String jobName) {
+        try {
+            //Set<Long> executions = jobOperator.getRunningExecutions(jobName)
+            //return !(executions?.isEmpty())
+
+            def result = batchSql.firstRow(
+                """
+                select count(bje.job_execution_id) as "executionCount" 
+                from ${batchTablePrefix}job_execution bje
+                inner join ${batchTablePrefix}job_instance bji on bje.job_instance_id = bji.job_instance_id
+                where bji.job_name = ?
+                and bje.status in ('STARTING', 'STARTED')""".toString(), [jobName]
+            )
+            return result['executionCount'] > 0
+        }
+        catch (Exception nsje) {
+            log.info("Failed to acquire running executions for $jobName", nsje)
+            return false
+        }
+    }
+
+    /**
+     * Launch a job
+     *
+     * @param jobName
+     * @param canBeConcurrent - when set to false, will look to see if this job
+     * is already executing and abort launch.  When true, it will always launch a new job
+     * @param jobParams
+     * @param jobLauncherName - initially configured choices "jobLauncher" and "syncJobLauncher"
+     * @return map containing whether the job was successful, and a message regarding either why it failed or what job was started
+     */
+    Map launch(String jobName, boolean canBeConcurrent = true, JobParameters jobParams = null, String jobLauncherName = null) {
+
+        // Is the app ready?
+        if (!ready) {
+            return [success: false,
+                message: "Attempted to launch $jobName, but app is not ready or job processing is disabled.",
+                failurePriority: 'low']
+        }
+
+        // Select Job
+        Job job
+        try {
+            job = jobRegistry.getJob(jobName)
+        }
+        catch (NoSuchJobException ignored) {
+            return [success: false, message: "Did not find Spring Batch Job: $jobName",
+                failurePriority: 'high']
+        }
+
+        // Can we run more than one at a time?
+        if (!canBeConcurrent && hasRunningExecutions(jobName)) {
+            return [success: false,
+                message: "Attempted to launch $jobName, but it is currently running.  Aborting launch.",
+                failurePriority: 'low']
+        }
+
+        // Select Job Launcher
+        JobLauncher selectedLauncher = null
+        if (jobLauncherName) {
+            selectedLauncher = grailsApplication.mainContext.getBean(jobLauncherName) as JobLauncher
+        }
+        if (!selectedLauncher) {
+            selectedLauncher = grailsApplication.mainContext.getBean(defaultJobLauncher) as JobLauncher
+            log.debug "JobLaucher $jobLauncherName selected but not found, trying $defaultJobLauncher"
+        }
+        if (!selectedLauncher) {
+            return [success: false, message: "Invalid jobLauncher $jobLauncherName selected", failurePriority: 'high']
+        }
+
+        // Select Job Parameters
+        if (!jobParams) {
+            log.debug "No job parameters provided for job, defaulting to date"
+            jobParams = mapToJobParameters(defaultJobParameters())
+        }
+
+        // Run the Job
+        JobExecution jobExecution
+        try {
+            jobExecution = selectedLauncher.run(job, jobParams)
+        }
+        catch (JobInstanceAlreadyCompleteException jiace) {
+            return [success: false, message: jiace.message, failurePriority: 'high']
+        }
+
+        return [
+            jobId: jobExecution.jobId, jobExecutionId: jobExecution.id,
+            success: true,
+            message: "Spring Batch Job($jobName) launched from EtlService"
+        ]
+    }
+
+    /**
+     * Job parameters to use by default
+     */
+    Map defaultJobParameters() {
+        ['date': new JobParameter(new Date().dateTimeString)]
+    }
+
+    /**
+     * Turn map of job parameters into Spring Batch JobParametersModel
+     */
+    JobParameters mapToJobParameters(Map map) {
+        return new JobParameters(map)
+    }
+
+    /**
+     * For job x, give me the last time it ran and whether it failed or not
+     *
+     * This method gives some of the same info as the SpringBatchUiService.jobModel,
+     * but might be more useful for automated monitoring.
+     */
+    Map jobStatus(String jobName) {
+        List<JobExecution> mostRecentJobExecutions = jobService.listJobExecutionsForJob(jobName, 0, 1)
+        if (!mostRecentJobExecutions) {
+            return [success: false]
+        }
+
+        JobExecution mostRecent = mostRecentJobExecutions[0]
+
+        boolean success = !mostRecent.status.isUnsuccessful()
+        boolean running = mostRecent.status.isRunning()
+        Date executionStartTime = mostRecent.startTime
+        Date executionEndTime = mostRecent.endTime
+
+        return [success: success, running: running, executionStartTime: executionStartTime, executionEndTime: executionEndTime]
+    }
+
+    /**
+     * Spring event listener to detect when the app is ready for job launching
+     */
+    @Override
+    void onApplicationEvent(ApplicationEvent event) {
+        if (event instanceof GrailsContextEvent) {
+            ready = true
+        }
+        if (event instanceof ContextClosedEvent) {
+            ready = false
+            log.info("No longer accepting Batch Jobs, shutting down.")
+        }
+    }
+
+
+    private DataSource _batchDataSource
+
+    private DataSource getBatchDataSource() {
+        _batchDataSource = _batchDataSource ?: grailsApplication.mainContext.getBean(grailsApplication.config.plugin.springBatch.dataSource ?: "dataSource")
+        return _batchDataSource
+    }
+    private String _batchTablePrefix
+
+    private String getBatchTablePrefix() {
+        if (null == _batchTablePrefix) {
+            _batchTablePrefix = grailsApplication.config.plugin.springBatch.tablePrefix ? (grailsApplication.config.plugin.springBatch.tablePrefix + '_') : 'BATCH_'
+        }
+        return _batchTablePrefix
+    }
+
+    private Sql getBatchSql() {
+        return new Sql(batchDataSource)
+    }
 }
